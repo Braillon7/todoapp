82898375fcf3c19d4c7578b56e683172a200bfab
William Watts <williamwatts38@gmail.com>
2018-01-18 03:31:14 +0000

    working

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.add-circle').on('click' , function(){
+    $('.task-form').addClass('active')
+  });
+  $('.task-form .close-btn').on('click' , function(){
+    $('.task-form').removeClass('active')
+ });
+});
